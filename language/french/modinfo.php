c3a0cc809222ee50a01456fd384c1dc46106d5c6
Camille Ollié <bosco.relly@laposte.net>
2016-05-21 02:07:34 +0200

    French language

---
+++ language/french/modinfo.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+/**
+ * wgXPiwik module for xoops
+ *
+ * @copyright       The XOOPS Project (http://xoops.org)
+ * @license         GPL 2.0 or later
+ * @package         wgxpiwik
+ * @since           1.0
+ * @min_xoops       2.5.7
+ * @author          Goffy - Wedega.com - Email:<webmaster@wedega.com> - Website:<http://wedega.com>
+ * @version         $Id: 1.0 modinfo.php 1 Tue 2016/01/12 23:05:11Z Goffy - Wedega $
+ */
+// ---------------- Admin Main ----------------
+define('_MI_WGXPIWIK_NAME', "wgXPiwik");
+// ---------------- Admin Menu ----------------
+define('_MI_WGXPIWIK_ADMENU1', "Tableau de bord");
+define('_MI_WGXPIWIK_ADMENU3', "Sites Piwik");
+define('_MI_WGXPIWIK_ADMENU4', "Utilisateurs Piwik");
+define('_MI_WGXPIWIK_ADMENU5', "Permissions");
+define('_MI_WGXPIWIK_ABOUT', "A propos");
+// ---------------- Admin Nav ----------------
+define('_MI_WGXPIWIK_ADMIN_PAGER', "Admin pager");
+define('_MI_WGXPIWIK_ADMIN_PAGER_DESC', "Admin per page list");
+// Submenu
+
+// ---------------- Blocks ----------------
+define('_MI_WGXPIWIK_TRACKER_BLOCK', "Tracker");
+
+// ---------------- Config ----------------
+define('_MI_WGXPIWIK_PIWIK_TRACKERTYPE', "Type de tracker");
+define('_MI_WGXPIWIK_PIWIK_TRACKERTYPE_JS', "Tracker JS");
+define('_MI_WGXPIWIK_PIWIK_TRACKERTYPE_PHP', "Tracker PHP");
+define('_MI_WGXPIWIK_PIWIK_TRACKERTYPE_IMG', "Tracker image");
+
+define('_MI_WGXPIWIK_PIWIK_PERIOD_DAY', "jour");
+define('_MI_WGXPIWIK_PIWIK_PERIOD_WEEK', "semaine");
+define('_MI_WGXPIWIK_PIWIK_PERIOD_MONTH', "mois");
+define('_MI_WGXPIWIK_PIWIK_DATE_YESTERDAY', "hier");
+define('_MI_WGXPIWIK_PIWIK_DATE_TODAY', "aujourd'hui");
+
+// ---------------- End ----------------
